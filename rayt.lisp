cc65dc199b8a5766abc1e8ece7f1a8491db9c6fe
martin <martin@hop.kcl.ac.uk>
2011-02-04 21:27:04 +0000

    get bfp image for each nucleus

--- rayt.lisp
+++ rayt.lisp
@@ -193,27 +193,41 @@ direction of excitation light)."
 	       (ray-does-not-hit-principal-sphere ())))))
        (asy "clip(unitsphere);")))))
 
-(defparameter *centers* ; in mm
-  (let* ((l '((3 212 168)
-	      (6 79 177)
-	      (12 125 111)
-	      (13 101 135)
-	      (13 155 89)
-	      (11 219 120)))
-	 (central 0)
-	 (offset (- (first (elt l central))))
-	 (a (make-array 
-	     (length l)
-	     :element-type 'vec
-	     :initial-contents
-	     (mapcar #'(lambda (q) 
-			 (destructuring-bind (z y x) q
-			   (declare (fixnum z y x))
-			   (v (* .01 (+ z offset))
-			      (* .001 (- y 150))
-			      (* .001 (- x 150)))))
-		     l))))
-    a))
+(defparameter *centers* nil) ;; in mm
+(defparameter *centers-fix* nil)
+(defparameter *data-dz-mm* 1s-3)
+(defparameter *data-dx-mm* .1s-3)
+(defparameter *data-width-px* 300)
+			
+(let* ((l '((3 212 168)
+	    (6 79 177)
+	    (12 125 111)
+	    (13 101 135)
+	    (13 155 89)
+	    (11 219 120)))
+       (central 0)
+       (offset 0 #+nil (- (first (elt l central))))
+       (a (make-array 
+	   (length l)
+	   :element-type 'vec
+	   :initial-contents
+	   (mapcar #'(lambda (q) 
+		       (destructuring-bind (z y x) q
+			 (declare (fixnum z y x))
+			 (v (* .01 (+ z offset))
+			    (* .001 (- y 150))
+			    (* .001 (- x 150)))))
+		   l)))
+       (afix (make-array (length l)
+			 :element-type 'vec
+			 :initial-contents
+			 (mapcar #'(lambda (q) 
+				     (destructuring-bind (z y x) q
+				       (declare (fixnum z y x))
+				       (v z y x)))
+				 l))))
+  (setf *centers* a
+	*centers-fix* afix))
 
 
 (defclass model ()
@@ -230,15 +244,16 @@ direction of excitation light)."
 		incf-vec)
 	 (inline incf-vec))
 (defun incf-vec (img n v val)
+  "Increase the value in an image at a position given by V."
   (declare (type (simple-array num 2) img)
-	   (fixnum n) (vec v)
-	   (num val))
+	   (type fixnum n) (type vec v)
+	   (type num val))
   (let* ((x (vx v))
 	 (y (vy v))
 	 (i (floor (* (+ 1s0 x) n) 2))
 	 (j (floor (* (+ 1s0 y) n) 2)))
-    (declare (num x y)
-	     (fixnum i j))
+    (declare (type num x y)
+	     (type fixnum i j))
     (incf (aref img j i) val)))
 
 (defun write-pgm (filename img)
@@ -274,21 +289,25 @@ direction of excitation light)."
   (declare (type (simple-array num 2) img))
   (destructuring-bind (h w) (array-dimensions img)
    (let* ((img1 (linear-array img))
-	  (ma (reduce #'max img1))
-	  (s (/ 255s0 ma))
 	  (buf (make-array (list h w) 
 			   :element-type '(unsigned-byte 8)
 			   :adjustable nil))
-	  (buf1 (linear-array buf)))
+	  (buf1 (linear-array buf))
+	  (ma (let ((e (reduce #'max img1)))
+		(unless e
+		  (return-from normalize-im buf))
+		e))
+	  (s (/ 255s0 ma)))
      (declare (type (simple-array (unsigned-byte 8) 2) buf)
 	      (type (array (unsigned-byte 8) 1) buf1))
      (dotimes (i (length buf1))
        (setf (aref buf1 i) (floor (* s (aref img1 i)))))
      (the (simple-array (unsigned-byte 8) 2) buf))))
 
-(defun trace-from-bfp (obj &optional (n 100))
-  (declare (vec obj)
-	   (fixnum n))
+(defun trace-from-bfp (obj nucleus &key (radius 2s-3) (w 100))
+  (declare (type vec obj)
+	   (type fixnum w)
+	   (num radius))
   (unless *pd*
     (setf *pd* (generate-poisson .01s0)))
   (let* ((f (find-focal-length 63s0))
@@ -297,12 +316,13 @@ direction of excitation light)."
 	(rif (* ri f))
 ;	(r (find-bfp-radius na f))
 	(pd *pd*)
-	(img (make-array (list n n) :element-type 'num
+	(img (make-array (list w w) :element-type 'num
 			 :adjustable nil
 			 :initial-element 0s0)))
    (declare (type (simple-array vec 1) pd))
    (dotimes (j (length pd))
-     (let ((bfp/r (aref pd j)))
+     (let ((bfp/r (aref pd j))
+	   (offset-mm (v 0 0 (vz (aref *centers* nucleus)))))
        (handler-case 
 	   (multiple-value-bind (dir hit)
 	       (ray-behind-objective obj bfp/r
@@ -310,17 +330,20 @@ direction of excitation light)."
 				     (v 1 0 0) 
 				     f na ri)
 	     (declare (ignore hit))
-	     (loop for i from 1 below (length *centers*) do
-		  (incf-vec img n bfp/r 
-			    (ray-sphere-intersection-length 
-			     obj (normalize dir) (aref *centers* i) 8s-3))))
+	     (loop for i below (length *centers*) do
+		  (unless (= i nucleus)
+		   (incf-vec img w bfp/r 
+			     (ray-sphere-intersection-length 
+			      obj dir
+			      (.- (aref *centers* i) offset-mm)
+			      radius)))))
 	 (ray-too-steep ())
 	 (ray-does-not-hit-principal-sphere ()))))
    (the (simple-array num 2) img)))
 
 
 (defmacro tmp (str &rest rest)
-  `(concatenate 'string "/home/martin/tmp/t0203/"
+  `(concatenate 'string "/home/martin/tmp/t0204/"
 		(format nil ,str ,@rest)))
 
 
@@ -346,18 +369,28 @@ direction of excitation light)."
 			 :initial-element 0s0)))
    (dotimes (j n)
      (dotimes (i n)
-       
-       
+        
        (let ((bfp (trace-from-bfp 
 		   (.- (.s s (v 0 j i))
 		       (.s (* .5s0 field) (v 0 1 1)))
-		   w)))
+		   0
+		   :radius 32s-3
+		   :w w)))
 	 (..+ sum bfp)
 	 (write-pgm (tmp "bfp~2,'0d-~2,'0d.pgm" j i)
 		    (normalize-im
 		     bfp)))))
    (write-pgm (tmp "sum.pgm")
 	      (normalize-im sum))))
+
+(declaim (inline set-aref))
+(defun set-aref (img j i val)
+  (declare (type (simple-array (unsigned-byte 8) 2) img)
+	   (type fixnum j i))
+  (destructuring-bind (h w) (array-dimensions img)
+    (when (and (<= 0 i (1- w))
+	       (<= 0 j (1- h)))
+      (setf (aref img j i) val))))
 
 (defun raster-line (img y x y1 x1 &optional (val 255))
   ;; wikipedia Bresenham's_line_algorithm
@@ -370,7 +403,7 @@ direction of excitation light)."
 	 (sy (if (< y y1) 1 -1))
 	 (e (- dx dy)))
     (loop
-	 (setf (aref img y x) val)
+       (set-aref img y x val) 
        (when (and (= x x1)
 		  (= y y1))
 	 (return-from raster-line
@@ -396,8 +429,8 @@ direction of excitation light)."
 	(y r))
     (declare (type fixnum f dx dy x y))
     (macrolet ((q (j i)
-		 `(setf (aref img (+ y0 ,j) (+ x0 ,i)) val
-			(aref img (+ y0 ,i) (+ x0 ,j)) val)))
+		 `(progn (set-aref img (+ y0 ,j) (+ x0 ,i) val)
+			 (set-aref img (+ y0 ,i) (+ x0 ,j) val))))
       (q r 0)
       (q (- r) 0)
       (loop while (< x y) do
@@ -455,3 +488,86 @@ direction of excitation light)."
   (raster-line m 12 13 150 190)
   (raster-disk m 200 200 40)
   (write-pgm "/dev/shm/o.pgm" m))
+
+
+(defun get-nuclei-in-slice (slice-px r-px)
+  "Return a list of nuclei that should be visible in the given slice."
+  (declare (type fixnum slice-px r-px))
+  (let ((res ()))
+   (dotimes (nucleus (length *centers-fix*))
+     (let ((c (aref *centers-fix* nucleus)))
+       (when (<= (abs (- slice-px (vz c))) r-px)
+	 (push nucleus res))))
+   res))
+
+(defun make-image (h &key (w h) (type '(unsigned-byte 8)))
+  (make-array (list h w)
+	      :element-type type))
+
+
+(defun draw-nucleus (img nucleus &key 
+		     (radius-mm 1.5s-3)
+		     (dx-mm 1s0))
+  (declare (type (simple-array (unsigned-byte 8) 2) img)
+	   (type fixnum nucleus)
+	   (type num radius-mm dx-mm))
+  (destructuring-bind (h w) (array-dimensions img)
+    (declare (ignore h))
+    (let ((s (/ (* 1s0 w) *data-width-px*)))
+      (setf dx-mm (* s *data-dx-mm*))
+      (let* ((c (aref *centers-fix* nucleus))
+	     ;(s (/ dx-mm *data-dx-mm*))
+	     (q (.s s c)) ;; note: vz wrong
+	     (x (floor (vx q)))
+	     (y (floor (vy q)))
+	     (rad (floor (* s radius-mm) *data-dx-mm*)))
+       (format t "~a~%" (list dx-mm 'dx-mm c q rad 's s ))
+       (raster-disk img y x rad))))
+  img)
+
+(defun draw-slice-through-point (slice-px &key 
+				 (radius-mm .003s0)
+				 (h 100) (w h)
+				 (dx-mm (* *data-dx-mm* (/ *data-width-px* w))))
+  (declare (type fixnum slice-px w h)
+	   (type num radius-mm dx-mm))
+  (let ((img (make-image h :w w)))
+    (dolist (e (get-nuclei-in-slice slice-px
+				    (ceiling radius-mm *data-dz-mm*)))
+      (draw-nucleus img e :radius-mm radius-mm :dx-mm dx-mm))
+    img))
+
+#+nil
+(dotimes (k 20)
+ (write-pgm (tmp "lcos~3,'0d.pgm" k)
+	    (draw-slice-through-point k :radius-mm 2.5e-3 :h 300)))
+
+(defun sum-bfp (nucleus &key (w-bfp 100) (w-ffp 30) (radius-ffp-mm 1.5s-3)
+		(radius-bfp-mm radius-ffp-mm))
+  "Average BFP exposure pattern for an area with radius RADIUS-FFP-MM
+on LCoS that illuminates the give NUCLEUS. The size of the projected
+spheres is defined by RADIUS-BFP-MM."
+  (let* ((ffp (make-image w-ffp))
+	 (bfp (make-image w-bfp :type 'num))
+	 (field (* *data-dx-mm* *data-width-px*))
+	 (s (/ field w-bfp)))
+    (draw-nucleus ffp nucleus :radius-mm radius-ffp-mm)
+    (dotimes (j w-ffp)
+      (dotimes (i w-ffp)
+	(when (< 0 (aref ffp j i))
+	  (..+ bfp
+	       (trace-from-bfp 
+		(.- (.s s (v 0 j i))
+		    (.s (* .5s0 field) (v 0 1 1)))
+		nucleus
+		:radius radius-bfp-mm
+		:w w-bfp)))))
+    (write-pgm (tmp "sumffp~3,'0d.pgm" nucleus) ffp)
+    (write-pgm (tmp "sumbfp~3,'0d.pgm" nucleus) (normalize-im bfp))))
+
+#+nil
+(dotimes (k (length *centers*))
+  (sum-bfp k 
+	   :radius-ffp-mm 2.4s-3
+	  :radius-bfp-mm 10s-3
+	  :w-bfp 300))
